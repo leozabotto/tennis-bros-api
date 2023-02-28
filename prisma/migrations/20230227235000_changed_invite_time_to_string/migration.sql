172e8f2d405edb3654d37331569f1abb74cc0458
Leonardo Zabotto <leozabotto02@gmail.com>
2023-02-28 08:34:00 -0300

    fix: prisma migrations to production deploy

--- prisma/migrations/20230227235000_changed_invite_time_to_string/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE `invite` MODIFY `time` VARCHAR(191) NOT NULL;
--- prisma/migrations/20230228113334_initial/migration.sql
+++ prisma/migrations/20230228113334_initial/migration.sql
@@ -1,8 +1,25 @@
 -- CreateTable
+CREATE TABLE `User` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+    `userName` VARCHAR(191) NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
+    `phoneNumber` VARCHAR(191) NOT NULL,
+    `password` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `User_userName_key`(`userName`),
+    UNIQUE INDEX `User_email_key`(`email`),
+    UNIQUE INDEX `User_phoneNumber_key`(`phoneNumber`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
 CREATE TABLE `Invite` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `date` DATE NOT NULL,
-    `time` TIME NOT NULL,
+    `time` VARCHAR(191) NOT NULL,
     `street` VARCHAR(191) NOT NULL,
     `addressNumber` VARCHAR(191) NOT NULL,
     `neighborhood` VARCHAR(191) NOT NULL,
@@ -19,7 +36,7 @@ CREATE TABLE `Invite` (
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
-CREATE TABLE `InviteInteraction` (
+CREATE TABLE `invite_interactions` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `type` ENUM('Aceito', 'Rejeitado') NOT NULL,
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
@@ -34,7 +51,7 @@ CREATE TABLE `InviteInteraction` (
 ALTER TABLE `Invite` ADD CONSTRAINT `Invite_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `InviteInteraction` ADD CONSTRAINT `InviteInteraction_inviteId_fkey` FOREIGN KEY (`inviteId`) REFERENCES `Invite`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `invite_interactions` ADD CONSTRAINT `invite_interactions_inviteId_fkey` FOREIGN KEY (`inviteId`) REFERENCES `Invite`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `InviteInteraction` ADD CONSTRAINT `InviteInteraction_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `invite_interactions` ADD CONSTRAINT `invite_interactions_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
